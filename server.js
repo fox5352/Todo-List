0673e2fb24a57e3d0fcd9f037d2dac54e455f19d
fox5352 <voschristopher53@gmail.com>
2023-06-06 19:44:07 +0200

    added about page and content and /login route

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const todoSchema = mongoose.Schema({
 
 const User = mongoose.model('users', todoSchema);
 
+
+
 // Express config
 const app = express();
 app.set('view engine', 'ejs');
@@ -29,24 +31,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Global vars
-let data = { navBtn: false };
+let data = { navBtn: false };// this var is used to dynamically change the one nav btn depending on what page your on
 
+
+// HOME PAGE
 app.route('/')
     .get(async function (req, res) {
         data.navBtn = false;
-        res.render('todoList', { data: data });
+        if (data.loggedIn) {
+            res.render('todoList', { data: data });
+        } else {
+            res.redirect('/about');
+        }
     });
 
+// ABOUT PAGE    
 app.route('/about')
     .get(async function (req, res) {
         data.navBtn = true;
-        res.render('todoList', { data: data });
+        res.render('about', { data: data });
     });
 
+// LOGIN PAGE
+app.route("/login")
+    .get(async function (req, res) {
+        res.end();
+    })
+    .post(async function (req, res) {
+        res.end();
+    });
+
+// CUSTOM ROUTE
 app.route("/:title")
     .get(async function (req, res) {
-        data.navBtn = false;
-        res.render('todoList', { data: data });
+        res.end('404');
     });
 
 app.listen(process.env.PORT, () => {
