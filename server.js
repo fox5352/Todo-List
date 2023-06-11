c9f6f174e608b2a50cb2a048d0dd49d269fd9cec
fox5352 <voschristopher53@gmail.com>
2023-06-11 18:12:22 +0200

    server.js code clean up and added comments

--- server.js
+++ server.js
@@ -37,31 +37,43 @@ app.use(express.static('public'));
 
 // Global vars
 let data = { navBtn: false, notes: [] };// this var is used to dynamically change the one nav btn depending on what page your on
-let userAccount = { userName: "default", password: "1234" };
-let counter = 0;
+let userAccount = { userName: "default" };
+let counter = 0;// to automatically display the about page if it's the default user account
 
-function changeUser(userName, password) {
-    userAccount = { userName: userName, password: password };
+// changes users name
+function changeUser(userName) {
+    userAccount = { userName: userName };
 }
 
 // HOME PAGE
 app.route('/')
+    // Handles home page
     .get(async function (req, res) {
+        //gets users data
         const response = await User.findOne({ userName: userAccount.userName });
 
+        // sets the navBtn to false which loads the about page button
         data.navBtn = false;
+        //sets the notes from the data base to the page
         data.notes = response.list;
 
-
-        res.render('todoList', { data: data });
-
-        // if (counter > 0) {
-        //     res.render('todoList', { data: data });
-        // } else {
-        //     counter++;
-        //     res.redirect('/about');
-        // }
+        // if its the default user account first redirect to about page
+        if (userAccount.userName === "default") {
+            if (counter > 0) {
+                // renders the home page
+                res.render('todoList', { data: data });
+            } else {
+                counter++;
+                // renders the about page
+                res.redirect('/about');
+            }
+        } else {
+            // renders the home page
+            res.render('todoList', { data: data });
+        }
     })
+    // sends data to the data base
+    // TODO: added validators later
     .post(async function (req, res) {
         const newData = req.body.newNote;
         const response = await User.updateOne({ userName: userAccount.userName }, { $push: { list: newData } });
@@ -70,63 +82,93 @@ app.route('/')
 
     });
 
+// handles the removal of the notes from the todo-list
+// TODO: pull data from index not string
 app.post('/remove', async function (req, res) {
+    // gets the users data
     const response = await User.findOne({ userName: userAccount.userName });
+    // this is just temporary till i can find a way to remove from index rather than the string its self
     const delItem = response.list[req.body.index];
 
+    // pull the data from the data base 
     const data = await User.updateOne({ userName: userAccount.userName }, { $pull: { list: delItem } });
 
+    // then reloads the page
     res.redirect('/');
 });
 
 // ABOUT PAGE    
 app.route('/about')
+    // renders the about page
     .get(async function (req, res) {
+        //sets the navBtn to home page then renders the page
         data.navBtn = true;
         res.render('about', { data: data });
     });
 
 // // LOGIN PAGE 
 app.route("/login")
+    // renders login form page
     .get(async function (req, res) {
         res.render('login', { data: data });
     })
+    //checks if the formData matches the data base then redirects
     .post(async function (req, res) {
         const response = await User.findOne({ userName: req.body.userName });
 
-        if (req.body.password === response.password) {
-            changeUser(req.body.userName, req.body.password);
+        // checks to see if username matches
+        if (response) {
+            //check if passwords matches
+            if (req.body.password === response.password) {
+                //then changes global user's name to new user
+                changeUser(req.body.userName);
 
-            res.redirect('/');
+                res.redirect('/');
+            } else {
+                res.redirect('/');
+            }
         } else {
-            res.redirect('/login');
+            res.redirect('/');
         }
     });
 
 app.route('/register')
+    //renders register page
     .get(async function (req, res) {
         res.render('register', { data: data });
     })
+    //validates then creates new users then redirects to home page
     .post(async function (req, res) {
+        // gets form data
         const data = req.body;
-        console.log(data.password1 === data.password2);
 
-        if (data.password1 === data.password2) {
-            const newUser = new User({
-                userName: data.userName,
-                password: data.password1
-            });
+        //validates if the userName exists already 
+        const userValidator = await User.exists({ userName: data.userName });
 
-            newUser.save()
-                .then(() => {
-                    changeUser(data.userName, data.password1);
 
-                    res.redirect('/');
+        if (!userValidator) {// if userName doesn't exist
+            // check if passwords match
+            if (data.password1 === data.password2) {
+                //create new document 
+                const newUser = new User({
+                    userName: data.userName,
+                    password: data.password1
                 });
 
+                newUser.save()//save then change global username to new user then redirect to home page
+                    .then(() => {
+                        changeUser(data.userName);
+
+                        res.redirect('/');
+                    });
+
+            } else {
+                res.redirect('/register');//passwords didn't match
+            }
         } else {
-            res.redirect('/register');
+            res.redirect('/register');//userName already exists
         }
+
     });
 
 app.listen(process.env.PORT, () => {
