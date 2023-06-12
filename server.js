a9bf4296dadf73f92f40c40930a35ef4eeac51e2
fox5352 <voschristopher53@gmail.com>
2023-06-12 09:39:14 +0200

    added hashing to users passwords and started on creating an alert system for forms

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-//TODO: istall mongoose-encryption
-
+// TODO: pull data from index not string in the remove route.
+// TODO: added validators later to the post / route.
 
 // DB Connection
 mongoose.connect(`${process.env.DB_URL}TodoListDB`, { useNewUrlParser: true });
@@ -28,8 +29,6 @@ const todoSchema = mongoose.Schema({
     }
 });
 
-//todoSchema.plugin(encrypt, {secret: key, encryptedFields: ['list','password']})
-
 // creates model
 const User = mongoose.model('users', todoSchema);
 
@@ -40,8 +39,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Global vars
-let data = { navBtn: false, notes: [] };// this var is used to dynamically change the one nav btn depending on what page your on
-let userAccount = { userName: "default" };
+const saltRounds = Number(process.env.SALT_ROUNDS);
+
+let data = { navBtn: false, notes: [], alert: false, alertMessage: '' };// this holds all the page data
+let userAccount = { userName: "default" };//loggedIn username
 let counter = 0;// to automatically display the about page if it's the default user account
 
 // changes users name
@@ -58,8 +59,12 @@ app.route('/')
 
         // sets the navBtn to false which loads the about page button
         data.navBtn = false;
-        //sets the notes from the data base to the page
-        data.notes = response.list;
+        try {
+            //sets the notes from the data base to the page
+            data.notes = response.list;
+        } catch (error) {
+            console.log(error);
+        }
 
         // if its the default user account first redirect to about page
         if (userAccount.userName === "default") {
@@ -77,7 +82,6 @@ app.route('/')
         }
     })
     // sends data to the data base
-    // TODO: added validators later
     .post(async function (req, res) {
         const newData = req.body.newNote;
         const response = await User.updateOne({ userName: userAccount.userName }, { $push: { list: newData } });
@@ -87,7 +91,6 @@ app.route('/')
     });
 
 // handles the removal of the notes from the todo-list
-// TODO: pull data from index not string
 app.post('/remove', async function (req, res) {
     // gets the users data
     const response = await User.findOne({ userName: userAccount.userName });
@@ -115,24 +118,41 @@ app.route("/login")
     // renders login form page
     .get(async function (req, res) {
         res.render('login', { data: data });
+
+        // if there was an alert pushed from a redirect it'll reset the values to default
+        if (data.alert === true) {
+            data.alert = false;
+            data.alertMessage = '';
+        }
     })
     //checks if the formData matches the data base then redirects
     .post(async function (req, res) {
+        // Gets the user data that matches
         const response = await User.findOne({ userName: req.body.userName });
 
         // checks to see if username matches
         if (response) {
-            //check if passwords matches
-            if (req.body.password === response.password) {
-                //then changes global user's name to new user
-                changeUser(req.body.userName);
 
-                res.redirect('/');
-            } else {
-                res.redirect('/');
-            }
+            // compares the users input to the databases data
+            bcrypt.compare(req.body.password, response.password, (err, bool) => {
+                if (!err) {
+                    // if passwords matches then changes username and redirects
+                    if (bool) {
+                        //then changes global user's name to new user
+                        changeUser(req.body.userName);
+
+                        res.redirect('/');
+                    } else {
+                        res.redirect('/login');
+                    }
+                } else {
+                    console.log(err);
+                    res.redirect('/login');
+                }
+            });
+
         } else {
-            res.redirect('/');
+            res.redirect('/login');
         }
     });
 
@@ -149,22 +169,32 @@ app.route('/register')
         //validates if the userName exists already 
         const userValidator = await User.exists({ userName: data.userName });
 
-
-        if (!userValidator) {// if userName doesn't exist
+        // if userName doesn't exist
+        if (!userValidator) {
             // check if passwords match
             if (data.password1 === data.password2) {
-                //create new document 
-                const newUser = new User({
-                    userName: data.userName,
-                    password: data.password1
+                // hashes the password before saving it
+                bcrypt.hash(data.password1, saltRounds, (err, hash) => {
+                    if (!err) {
+                        //create new user document 
+                        const newUser = new User({
+                            userName: data.userName,
+                            password: hash
+                        });
+
+                        //save then change global username to new user then redirect to home page
+                        newUser.save()
+                            .then(() => {
+                                changeUser(data.userName);
+
+                                res.redirect('/');
+                            });
+
+                    } else {
+                        console.log(err);
+                        res.redirect('/register');
+                    }
                 });
-
-                newUser.save()//save then change global username to new user then redirect to home page
-                    .then(() => {
-                        changeUser(data.userName);
-
-                        res.redirect('/');
-                    });
 
             } else {
                 res.redirect('/register');//passwords didn't match
